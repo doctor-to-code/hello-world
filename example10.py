8f80abc4a5b1172b89ad65a85c1bdd2d8a66803f
Peter Yang <fgdeeerry@gmail.com>
2022-02-10 01:07:56 +0800

    4th edition

--- example10.py
+++ example10.py
@@ -29,3 +29,18 @@ Location: 板橋
 //               佛祖保佑         永無BUG
 
 """
+
+#真實邏輯運算實際案例
+while True:
+    a = int(input('請輸入數字'))
+    if a == 0:
+        break
+    x1 = a > 50
+    x2 = a % 4 == 0
+    x3 = a ** 2 > 3000
+    x4 = (x1 and x2)
+    x5 = (x1 or x2)
+    x6 = (not x2)
+    x7 = (not x3)
+    x8 = (not x6 and not x7)
+    print(x1, x2, x3, x4, x5, x6, x7, x8)
