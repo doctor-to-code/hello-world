3feafa9e6ef7ae33bc6f7cefe3abd9664ad3fedb
Peter Yang <fgdeeerry@gmail.com>
2022-02-10 01:08:20 +0800

    4th edition

--- example09.py
+++ example09.py
@@ -29,3 +29,37 @@ Location: 板橋
 //               佛祖保佑         永無BUG
 
 """
+#{python運算符號簡介}
+#賦值運算: 右邊的數值賦予給左邊(變量)
+#算數運算: 加減乘除
+#複合的賦值運算: +=, -=, *=, /=
+#關係運算: >, <, <=, >=, ==(等於，一個等號叫做賦值), !=(不等於)
+#邏輯運算: and(且), or(或), not(非)，用於把多個bool值處理成一個bool值
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+print('-' * 10)
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+print('-' * 10)
+print(not True)
+print(not False)
+#crtl + / 可以註釋掉程序
+
+# a = 3
+# b = 5
+# c = 4
+# #a = a+ b
+# a += b
+#
+# #c乘上(b + 2)= 4 * (5 + 2)=28
+# c *= b + 2
+# print(a, c)
+# print(a > b)
+# print(a <= b)
+# print(a >= b)
+# print(a == b)
+# print(a != b)
