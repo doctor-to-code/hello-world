a320b3bf756384a430c62d69ae81fb07cebbc32c
Peter Yang <fgdeeerry@gmail.com>
2022-02-09 15:07:45 +0800

    example06 1st commit

---
+++ example02.py
@@ -0,0 +1,30 @@
+"""
+Python 課程
+Author: PY
+Date: 2022/02/08
+Location: 板橋
+"""
+
+print('hello, world', end='***')
+print('goodbye')
+print(125 - 5)
+print(125 + 5)
+print(125 * 5)
+print(125 / 5)
+print(125 // 6)
+print(125 % 5)
+#求冪次方125之5次方
+print(125 ** 5)
+
+x = 125
+y = 5
+print(x + y)
+print(x - y)
+print(x / (y + 5))
+print(y / x)
+print(y // x)
+print(x ** (y + 10))
+print(x % (y + 2))
+student_age = 18
+#變量命名有多個單詞連接時候用下滑線分隔，稱作snake case
+
---
+++ example04.py
@@ -0,0 +1,18 @@
+"""
+Python 課程
+Author: PY
+Date: 2022/02/09
+Location: 板橋
+"""
+
+a = 5
+b = 9.5
+c = 'hello'
+d = 1 > 2
+e = 3 + 5j
+
+print(a, type(a)) # int
+print(b, type(b)) # float
+print(c, type(c)) # str
+print(d, type(d)) # bool
+print(e, type(e)) # complex
