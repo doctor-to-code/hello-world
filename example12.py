5fd022fd4861d893b1af6b2e54652619382e1b7a
Peter Yang <fgdeeerry@gmail.com>
2022-02-10 01:06:41 +0800

    4th edition

--- example12.py
+++ example12.py
@@ -29,3 +29,17 @@ Location: 板橋
 //               佛祖保佑         永無BUG
 
 """
+#三角形形成計算器:
+while True:
+    a = float(input('三角形邊長a'))
+    b = float(input('三角形邊長b'))
+    c = float(input('三角形邊長c'))
+    x1 = a + b > c
+    x2 = b + c > a
+    x3 = a + c > b
+    y = (x1 and x2 and x3)
+    if y == True:
+        print('可組成三角形')
+    else:
+        print('無法組成三角形')
+
