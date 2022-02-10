46a507370962c94048c0523a664ca23d1f35145e
Peter Yang <fgdeeerry@gmail.com>
2022-02-10 15:55:15 +0800

    6th edition

--- example14.py
+++ example14.py
@@ -29,3 +29,16 @@ Location: 板橋
 //               佛祖保佑         永無BUG
 
 """
+
+username = input('請輸入使用者名稱:')
+password = input('請輸入密碼:')
+if username == 'fgdeeerry' and password == 'fgdeerry':
+    print('登錄成功，歡迎使用餘額查詢系統，請進行下一步操作')
+    x = float(input('請輸入網域密碼'))
+    if x != 0:
+        print('您的網域密碼為', x, '顯示餘額為', x * 100, '感謝使用餘額查詢系統')
+    else:
+        print('已退出系統，感謝您的使用')
+else:
+    print('登錄失敗，請確認帳號密碼是否有誤')
+    print('已退出系統，感謝您的使用')
