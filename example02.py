806fa6048d26cad6672af18f00c35fc9e3b29b2d
Peter Yang <fgdeeerry@gmail.com>
2022-02-23 11:36:16 +0800

    public edition

--- example02.py
+++ example02.py
@@ -11,20 +11,20 @@ print(125 - 5)
 print(125 + 5)
 print(125 * 5)
 print(125 / 5)
-print(125 // 6)
-print(125 % 5)
-#求冪次方125之5次方
-print(125 ** 5)
+print(125 // 6) # //是指整除部分
+print(125 % 5) # %是求餘數意思
+print(125 ** 5) #求冪次方125之5次方
 
-x = 125
-y = 5
+x = int(input('輸入整數'))
+y = int(input('輸入整數'))
 print(x + y)
 print(x - y)
 print(x / (y + 5))
 print(y / x)
 print(y // x)
-print(x ** (y + 10))
+print(x ** (y + 5))
 print(x % (y + 2))
+print(x ** y + 5)
 student_age = 18
 #變量命名有多個單詞連接時候用下滑線分隔，稱作snake case
 
